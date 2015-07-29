cdf1e2d714d2f310d7616db26cd92d2abb73033c
newshell11 <zhalrhd@gmail.com>
2015-07-29 15:47:06 +0900

    new Activity of recorder

--- app/src/main/java/com/example/sora/mymusic/MainActivity.java
+++ app/src/main/java/com/example/sora/mymusic/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sora.mymusic;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Intent intentSubActivity =
+                        new Intent(MainActivity.this, AudioRecordTest.class);
+                startActivity(intentSubActivity);
+            }
+        });
     }
 
     @Override
